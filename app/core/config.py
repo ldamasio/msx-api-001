7a752e61ef63690b1841dcead4c61fd205ffee78
Leandro Damasio <ldamasio@gmail.com>
2024-09-21 02:45:21 -0300

    feat(config): Introduce pydantic-settings for centralized settings management

--- app/core/config.py
+++ app/core/config.py
@@ -1,9 +1,20 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
 import os
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 dotenv_path = os.path.join(BASE_DIR, '.env')
 load_dotenv(dotenv_path)
+
+print (BASE_DIR)
+print (os.getenv('DATABASE_URL'))
+print (os.getenv('SECRET_KEY'))
+print (os.getenv('ALGORITHM'))
+print (os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES'))
+print (int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES')))
+print (type(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES')))
+print (type(int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES'))))
+
 
 class Settings(BaseSettings):
     DATABASE_URL: str = os.getenv('DATABASE_URL')
