7e59ce3b4553f20607eea750abc74f1795fa0f83
Leandro Damasio <ldamasio@gmail.com>
2024-09-22 17:07:15 -0300

    feat: Include user route to main file

--- app/main.py
+++ app/main.py
@@ -4,34 +4,22 @@ from fastapi import FastAPI
 from app.db.session import engine
 from app.db.base import Base  # Importa a Base com os modelos
 # from app.core.auth import get_current_user
-from app.api.api_v1.vehicles import router
-
+from app.api.api_v1.vehicles import router as vehicle_router
+from app.api.api_v1.users import router as user_router
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
 )
 
-# Configurações CORS (Cross-Origin Resource Sharing)
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],  # Em produção, defina domínios específicos
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+Configurações CORS (Cross-Origin Resource Sharing)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Em produção, defina domínios específicos
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-
-# Inclui o roteador principal da API (com autenticação/authorization)
-# app.include_router(router, prefix=settings.API_V1_STR)
-
-
-
-# # Exemplo de rota protegida por autenticação
-# @app.get("/secure-data", dependencies=[Depends(get_current_user)])
-# async def get_secure_data():
-#     return {"message": "This is a protected route. User is authenticated."}
-
-
-
-app.include_router(router, prefix="/vehicles", tags=["Vehicles"])
+app.include_router(vehicle_router, prefix="/vehicles", tags=["Vehicles"])
+app.include_router(user_router, prefix="/api/v1/users", tags=["Users"])
