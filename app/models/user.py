51fcda121baaa8eb4267bc04ab0f047183076328
Leandro Damasio <ldamasio@gmail.com>
2024-09-22 16:36:52 -0300

    feat: Add user model

---
+++ app/models/user.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.sql import func
+from app.db.base import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    created_at = Column(func.now())
