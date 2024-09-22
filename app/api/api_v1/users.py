d7278f63a3bea23cf6fafed805fde68032dfd571
Leandro Damasio <ldamasio@gmail.com>
2024-09-22 17:33:29 -0300

    feat: Add token generator

--- app/api/api_v1/users.py
+++ app/api/api_v1/users.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from app.db.session import get_db
 from app.models.user import User
-from app.schemas.user import UserCreate, UserRead
-from app.core.security import hash_password
-from fastapi.security import OAuth2PasswordBearer
+from app.schemas.user import UserCreate, UserRead, Token
+from app.core.security import hash_password, verify_password
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 import jwt
 from app.core.config import settings
 from datetime import datetime, timedelta
@@ -24,7 +24,6 @@ def create_access_token(data: dict):
 
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     try:
-        # Decodifica o token usando a chave secreta definida nas configurações
         payload = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=[settings.JWT_ALGORITHM])
         user_id = payload.get("sub")  # Assume que o ID do usuário está no campo 'sub'
         
@@ -41,10 +40,24 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
     except jwt.PyJWTError:
         raise HTTPException(status_code=401, detail="Invalid authentication credentials")
 
+# Rota para gerar o token JWT
+@router.post("/token", response_model=Token)
+def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.username == form_data.username).first()
+    
+    if not user or not verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    
+    access_token = create_access_token(data={"sub": user.id})  # Use o ID do usuário como 'sub'
+    return {"access_token": access_token, "token_type": "bearer"}
+
 # Rota para adicionar um novo usuário
 @router.post("/", response_model=UserRead)
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
-    # Hash da senha usando a função definida em security.py
     hashed_password = hash_password(user.password)
     db_user = User(username=user.username, hashed_password=hashed_password)
     
@@ -77,8 +90,8 @@ def change_password(new_password: str, token: str = Depends(oauth2_scheme), db: 
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
 
-    # Hash da nova senha usando a função definida em security.py
     user.hashed_password = hash_password(new_password)
     db.commit()
     
     return {"message": "Password updated successfully"}
+
